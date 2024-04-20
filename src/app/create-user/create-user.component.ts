e6d8d89b0be379d1ec37d369d90c0420401545a9
Borsoi <borsoi.guilherme@gmail.com>
2024-04-19 23:12:56 -0300

    Melhorado validação da data de nascimento e adicionado lógica para evitar username iguais entre usuários

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -25,21 +25,32 @@ export class CreateUserComponent {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       cpf: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
-      dataNascimento: ['', [Validators.pattern(/^\d{8}$/)]],
+      dataNascimento: ['', Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])(19\d{2}|20[0-9]{2})$/)],
     })
   }
 
   registerNewUser() {
     if (this.userForm.valid) {
-      this.saveUser();
-      this.router.navigate(['/login']);
+      const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+      const newUser = this.getNewUserInput();
+      if (this.validateUsernameAvailable(newUser, users)) {
+        this.saveUser(newUser, users);
+        this.router.navigate(['/login']);
+      } else {
+        alert('O nome de usuário não está disponível, escolha outro nome de usuário')
+      }
     } else {
       alert('Existem erros no formulário, verifique e tente novamente');
     }
   }
 
-  saveUser() {
-    const newUser: User = {
+  saveUser(newUser: User, users: User[]) {
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  getNewUserInput(): User {
+    return {
       id: this.getNextUserId(),
       username: this.userForm.value.username,
       email: this.userForm.value.email,
@@ -47,10 +58,14 @@ export class CreateUserComponent {
       cpf: this.userForm.value.cpf,
       dataNascimento: this.userForm.value.dataNascimento
     };
+  }
 
-    const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
-    users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
+  validateUsernameAvailable(newUser: User, users: User[]): boolean {
+    const existingUser = users.find(user => user.username === newUser.username)
+    if (existingUser) {
+      return false;
+    }
+    return true;
   }
 
   getNextUserId(): number {
